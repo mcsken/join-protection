53e75d5da96351317601a357c596a2afd39980e2
Mek Skëndo <mekskendo@gmail.com>
2022-09-25 19:29:14 +0200

    #1 Clean up code from merge

--- src/main/java/me/mcsken/joinprotection/JoinProtection.java
+++ src/main/java/me/mcsken/joinprotection/JoinProtection.java
@@ -5,7 +5,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
@@ -20,11 +19,6 @@ public final class JoinProtection extends JavaPlugin implements Listener {
     private boolean joinMovement;
     private String joinMessageStart;
     private String joinMessageEnded;
-    private boolean dimensionEnable;
-    private int dimensionSeconds;
-    private boolean dimensionMovement;
-    private String dimensionMessageStart;
-    private String dimensionMessageEnded;
     public static final int TICKS_PER_SECOND = 20;
     public static final float STANDARD_WALK_SPEED = 0.2F;
 
@@ -47,16 +41,6 @@ public final class JoinProtection extends JavaPlugin implements Listener {
         this.joinMessageEnded = ChatColor.translateAlternateColorCodes('&',
                 Objects.requireNonNull(getConfig().getString("Messages.joinCooldownEnded"))
                         .replace("%time%", Integer.toString(this.joinSeconds)));
-
-        this.dimensionEnable = getConfig().getBoolean("dimensionEnableProtection");
-        this.dimensionSeconds = getConfig().getInt("dimensionTimeProtection");
-        this.dimensionMovement = getConfig().getBoolean("dimensionMovementAllow");
-        this.dimensionMessageStart = ChatColor.translateAlternateColorCodes('&',
-                Objects.requireNonNull(getConfig().getString("Messages.dimensionCooldownStart"))
-                        .replace("%time%", Integer.toString(this.dimensionSeconds)));
-        this.dimensionMessageEnded = ChatColor.translateAlternateColorCodes('&',
-                Objects.requireNonNull(getConfig().getString("Messages.dimensionCooldownEnded"))
-                        .replace("%time%", Integer.toString(this.dimensionSeconds)));
 
         try {
             if (!Objects.requireNonNull(getConfig().getString("Version")).equalsIgnoreCase("0.1.1")) {
@@ -87,28 +71,6 @@ public final class JoinProtection extends JavaPlugin implements Listener {
                     player.removePotionEffect(PotionEffectType.JUMP);
                 }
             }, (long) this.joinSeconds * TICKS_PER_SECOND);
-        }
-    }
-    @EventHandler
-    public void onDimensionChange(PlayerChangedWorldEvent event) {
-        final Player player = event.getPlayer();
-        if (this.dimensionEnable && player.hasPermission("enableProtection.dimension") && !player.hasPermission("disableProtection.dimension")) {
-            Bukkit.getScheduler().scheduleSyncDelayedTask(getInstance(), () -> {
-                player.sendMessage(JoinProtection.this.dimensionMessageStart);
-                player.setNoDamageTicks(JoinProtection.this.dimensionSeconds * TICKS_PER_SECOND);
-                if (!JoinProtection.this.dimensionMovement) {
-                    player.setWalkSpeed(0.0F);
-                    player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 72000, 128, false, false, false));
-                }
-            });
-
-            Bukkit.getScheduler().scheduleSyncDelayedTask(getInstance(), () -> {
-                player.sendMessage(JoinProtection.this.dimensionMessageEnded);
-                if (!JoinProtection.this.dimensionMovement) {
-                    player.setWalkSpeed(STANDARD_WALK_SPEED);
-                    player.removePotionEffect(PotionEffectType.JUMP);
-                }
-            }, (long) this.dimensionSeconds * TICKS_PER_SECOND);
         }
     }
 }
